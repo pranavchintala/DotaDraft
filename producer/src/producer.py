b676e599c9f3f50276185a69fca9cec85c76832f
Ubuntu <ubuntu@ip-10-0-0-7.ec2.internal>
2018-10-15 19:51:12 +0000

    Updated producer script

--- producer/src/producer.py
+++ producer/src/producer.py
@@ -1,60 +1,34 @@
 import boto3
-from io import BytesIO
 import json
-import sys
 import time
+import random
+import pandas
+import datetime
 
-def get_kinesis_record(item):
-       # item = {'hashKey': randrange(0, 5000000), 'sortKey': str(uuid.uuid4()), 'created': datetime.datetime.utcnow().isoformat()}
-
-        raw_data = json.dumps(item)
-        encoded_data = bytes(raw_data)
-        kinesis_record = {
-            'Data': encoded_data,
-            'PartitionKey': str(item['hashKey']),
-        }
-
-        return kinesis_record
-
-
-
+def isValidGame(game_modes,human_players,duration):
+    if game_modes[0] in (1,2,3,5,16,22) and human_players[0] == 10 and duration[0]>780:
+	return True
+    return False
+def generate_kinesis_record(item):
+	JSON = json.dumps(info)
+	return JSON
 
 
 client = boto3.client(
 's3',
 aws_access_key_id=AWS_ACCESS_KEY,
-aws_secret_access_key=AWS_SECRET_KEY)
+aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
 
-
-client = boto3.client('s3')
 
 kinesis_client = boto3.client('kinesis', region_name='us-east-1')
-print kinesis_client.describe_stream(StreamName = "TestStream")
-print "Collecting data from bucket..."
-#datafroms3 = client.get_object(Bucket = 'dotadatastorage', Key = 'match_skill.gz')
-datafroms3 = client.get_object(Bucket = 'dotadatastorage', Key = 'matches')
-print type(datafroms3['Body'])
-print "DONE"
-i=1
-print "Starting Streaming!"
-while True:
-	bytestream = datafroms3['Body'].read(amt=512)
-	print bytestream
-	put_response = kinesis_client.put_record(StreamName = "TestStream", Data = bytestream,PartitionKey = str(i))
-	i=i+1
-	if i>500:
-		i=1
-'''
-while True:
-	bytestream = datafroms3['Body'].iter_lines(chunk_size=10)
-	for line in bytestream:
-		print line
-		put_response = kinesis_client.put_record(StreamName = "TestStream", Data = line,PartitionKey = str(i))
-		i=i+1
-	if i>500:
-		i=1
-	time.sleep(1)
-        print "Sending stream to kinesis"
-        #put_response = kinesis_client.put_record(StreamName = "TestStream", Data = bytestream,PartitionKey = str(i))
-        #print put_response
-'''
+client = boto3.client('s3')
+kinesis_stream_name = "Consumer_Test"
+
+#Put records in one at a time into the Kinesis Stream. One thread provides ~100 games per second
+for line in pandas.read_csv("s3://dotadatastorage/matches", chunksize = 1):
+	info = line.to_dict(orient = "list")
+	info["Kinesis_Stream_Timestamp"] = datetime.datetime.utcnow().isoformat()
+	game_validity = isValidGame(info['game_mode'],info['human_players'],info['duration'])
+	if game_validity:
+		#We use a partition key which results in uniform partitioning into Kinesis
+		put_response = kinesis_client.put_record(StreamName = kinesis_stream_name, PartitionKey = str(random.randrange(0, 5000000)), Data = generate_kinesis_record(info))
